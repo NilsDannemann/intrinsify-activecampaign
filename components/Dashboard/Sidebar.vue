989d75b0ae8af982e4889f1dc99943696353f143
Nils Dannemann <ndannemann@gmail.com>
2022-09-22 12:50:59 +0400

    Quick fixes to dashboard layout

--- components/Dashboard/Sidebar.vue
+++ components/Dashboard/Sidebar.vue
@@ -38,9 +38,9 @@ export default defineComponent({
             class="group flex items-center mb-4 hover:no-underline"
           >
             <div
-              class="flex items-center mr-4 px-2 py-2 rounded-md ring-1 ring-slate-900/5 shadow-sm group-hover:shadow group-hover:ring-slate-900/10 dark:ring-0 dark:shadow-none dark:group-hover:shadow-none dark:group-hover:highlight-white/10 group-hover:shadow-sky-200 dark:highlight-white/10"
+              class="flex items-center mr-4 px-2 py-2 rounded-sm dark:group-hover:highlight-white/10 dark:highlight-white/10"
               :class="{
-                'text-white dark:text-white group-hover:bg-sky-500 bg-sky-500':
+                'text-white dark:text-white group-hover:bg-primary-500 bg-primary-500':
                   i === 1,
                 'text-slate-500 dark:text-gray-100 group-hover:bg-gray-200 bg-gray-100 dark:group-hover:bg-slate-600 dark:bg-slate-700':
                   i !== 1,
@@ -51,7 +51,7 @@ export default defineComponent({
             <span
               class="text-sm font-semibold capitalize"
               :class="{
-                'font-extrabold text-sky-500 dark:text-sky-400': i === 1,
+                'font-extrabold text-primary-500 dark:text-primary-400': i === 1,
               }"
             >
               {{ $t('pages.dashboard.index.nav') }}
