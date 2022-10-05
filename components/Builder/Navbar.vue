7276722a6dbff94c9329e43d10d6e8da93cea442
Nils Dannemann <ndannemann@gmail.com>
2022-10-05 12:49:35 +0400

    Update Navbar.vue

--- components/Builder/Navbar.vue
+++ components/Builder/Navbar.vue
@@ -49,10 +49,7 @@ const toggleOptions = (show?: boolean) => {
 </script>
 
 <template>
-  <div
-    ref="navbar"
-    class="backdrop-filter backdrop-blur-md top-0 z-40 w-full flex-none transition-colors duration-300 lg:z-50 border-b border-gray-900/10 dark:border-gray-50/[0.2] bg-white/[0.5] dark:bg-slate-900/[0.5]"
-  >
+  <div ref="navbar" class="app-nav backdrop-filter backdrop-blur-md top-0 z-40 w-full flex-none transition-colors duration-300 lg:z-50 border-b border-gray-900/10 dark:border-gray-50/[0.2] bg-white/[0.5] dark:bg-slate-900/[0.5]">
     <div id="navbar-banner" class="banner">
       <slot name="banner" />
     </div>
@@ -165,14 +162,14 @@ html.dark .app-logo {
   opacity: 0;
 }
 
-a.router-link-active {
+.app-nav a.router-link-active {
   font-weight: 600;
 }
-a.router-link-exact-active {
+.app-nav a.router-link-exact-active {
   color: theme('colors.slate.900');
 }
 html.dark {
-  a.router-link-exact-active {
+  .app-nav a.router-link-exact-active {
     color: theme('colors.white');
   }
 }
