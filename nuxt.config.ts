c75d8e01024a9054a3c47c82db828703623858ba
Nils Dannemann <ndannemann@gmail.com>
2022-10-05 11:06:07 +0400

    Cleanup + added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,14 +17,14 @@ export default defineNuxtConfig({
   // app
   app: {
     head: {
-      title: 'Nuxt 3 Awesome Starter',
-      titleTemplate: '%s - Nuxt 3 Awesome Starter',
+      title: 'intrinsify Akademie',
+      titleTemplate: '%s - intrinsify Akademie',
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
           hid: 'description',
           name: 'description',
-          content: 'Nuxt 3 Awesome Starter',
+          content: 'intrinsify Akademie',
         },
       ],
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
