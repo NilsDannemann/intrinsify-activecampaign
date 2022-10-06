03e2bfa1d0dd74cae20955e689fb747f230b89f6
Nils Dannemann <ndannemann@gmail.com>
2022-10-06 14:04:05 +0400

    minor change

--- content/news/akademie-update-oktober.md
+++ content/news/akademie-update-oktober.md
@@ -2,7 +2,7 @@
 title: 'Akademie-Update: Oktober'
 description: 'Lorem ipsum dolor sit amet, sadipscing elitr, sed eirmod amet.'
 date: '29.10.2022'
-author: 'Team intrinsify'
+author: 'Lars'
 ---
 
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -31,18 +31,15 @@ useHead(() => ({
             class="block hover:no-underline p-6 flex space-x-6 rounded border border-gray-900/10 dark:border-gray-50/[0.2]"
           >
             <div class="w-30">
-              <div class="text-xl font-semibold text-slate-800 dark:text-gray-50">{{ article.date }}</div>
-              <Anchor
-                class="text-sm flex items-center space-x-1"
-                :href="`https://www.github.com/${article.author}`"
-              >
+              <div class="text-lg mb-1 font-semibold text-slate-800 dark:text-gray-50">{{ article.date }}</div>
+              <div class="text-sm flex items-center space-x-1">
                 <img
-                  class="w-5 h-5 rounded-full"
+                  class="w-4 h-4 rounded-full"
                   src="~/assets/images/avatar-mark.jpeg"
                   :alt="article.author"
                 />
                 <span>{{ article.author }}</span>
-              </Anchor>
+              </div>
             </div>
             <div class="flex flex-col">
               <Anchor
